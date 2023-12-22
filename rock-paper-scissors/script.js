65868fe1ec88fbe49862035e36f7e31cc4b37951
salce0926 <kit.kny2014@gmail.com>
2023-12-23 01:31:26 +0900

    ユーザー通知の実装

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,21 +1,35 @@
 const ws = new WebSocket('wss://nodepractice-e56g.onrender.com/');
 
-    ws.addEventListener('open', (event) => {
-      console.log('WebSocket connection opened');
-    });
+ws.addEventListener('open', (event) => {
+  console.log('WebSocket connection opened');
 
-    ws.addEventListener('message', (event) => {
-      const { result } = JSON.parse(event.data);
-      displayResult(result);
-    });
+  // クライアントが接続した際にユーザー名をサーバーに送信
+  const username = prompt('Enter your username:');
+  ws.send(JSON.stringify({ type: 'join', username }));
+});
 
-    async function playGame() {
-      const playerChoice = document.getElementById('choice').value;
+ws.addEventListener('message', (event) => {
+  const data = JSON.parse(event.data);
 
-      ws.send(JSON.stringify({ playerChoice }));
-    }
+  if (data.type === 'userList') {
+    // サーバーからのユーザーリストを受信したときの処理
+    const usernames = data.usernames;
+    console.log('User list:', usernames);
+  } else {
+    // じゃんけんの結果などの他のメッセージの処理
+    const { result } = data;
+    displayResult(result);
+  }
+});
 
-    function displayResult(result) {
-      const resultElement = document.getElementById('result');
-      resultElement.textContent = result;
-    }
+async function playGame() {
+  const playerChoice = document.getElementById('choice').value;
+
+  // じゃんけんの手をサーバーに送信
+  ws.send(JSON.stringify({ type: 'game', playerChoice }));
+}
+
+function displayResult(result) {
+  const resultElement = document.getElementById('result');
+  resultElement.textContent = result;
+}
