0f6ca8cba3ffa3e0c90bb2938ffa3c53aaf8249b
salce0926 <kit.kny2014@gmail.com>
2024-06-28 18:29:51 +0900

    ブラインド修正

--- poker-blind-timer/script.js
+++ poker-blind-timer/script.js
@@ -4,27 +4,19 @@ let blinds = [
     {small: 300, big: 600},
     {small: 400, big: 800},
     {small: 500, big: 1000},
-    {small: 600, big: 1200},
     {small: 700, big: 1400},
-    {small: 800, big: 1600},
     {small: 1000, big: 2000},
+    {small: 1500, big: 3000},
     {small: 2000, big: 4000},
     {small: 3000, big: 6000},
-    {small: 4000, big: 8000},
     {small: 5000, big: 10000},
-    {small: 6000, big: 12000},
     {small: 7000, big: 14000},
-    {small: 8000, big: 16000},
     {small: 10000, big: 20000},
     {small: 15000, big: 30000},
     {small: 20000, big: 40000},
-    {small: 25000, big: 50000},
     {small: 30000, big: 60000},
-    {small: 35000, big: 70000},
     {small: 40000, big: 80000},
-    {small: 45000, big: 90000},
     {small: 50000, big: 100000},
-    // 追加のブラインドレベルをここに追加できます
 ];
 
 let currentLevel = 0;
